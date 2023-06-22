4910ba501bee6915b4cc1cee08857ddea41421c4
zionimo <zionimobu@gmail.com>
2023-06-23 08:22:45 +0900

    favicon 수정중

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
 
       <body className={inter.className}>{children}</body>
